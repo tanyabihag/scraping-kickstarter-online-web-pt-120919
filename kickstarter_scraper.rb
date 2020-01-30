2d93ecc8113ed17f65ab819605725ab6c7eaf93d
Tanya Bihag <tbihag@gmail.com>
2020-01-30 04:43:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,9 +4,17 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
-end
- 
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do
+    |project|
+    title = project.css(h2.bbcard_name strong a").text 
+    projects[project] = {}
+  end
+  
+  projects
+end 
 create_project_hash
 
 
